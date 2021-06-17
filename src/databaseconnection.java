510f20105cd4af1afdd27b84ebfdab3ad3fdc185
Ossama Mehmood <ossamamehmood110@gmail.com>
2021-06-17 19:04:23 +0500

    Commit

---
+++ src/databaseconnection.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class databaseconnection {
+    //C:\Program Files\Java\jdk1.8.0_281\jre\lib\ext
+    final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final static String DB_URL = "jdbc:mysql://localhost:3306/registrationmanagementsystem";
+    
+    final static String USER = "root";
+    final static String PASS = "";
+    
+    public static Connection connection(){
+        try{
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            return conn;
+        }catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+    
+}
